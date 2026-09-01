323eaed2616a1738be24b97a2e6b07d2aaa9f0a8
scher <schermann470@outlook.com>
2026-09-01 19:11:05 +0300

    Add vdoc

---
+++ docs/search_index.js
@@ -0,0 +1,60 @@
+var searchModuleIndex = [
+"i18n",
+];
+
+var searchIndex = [
+"Bundle",
+"default_language",
+"language_tags",
+"add_messages",
+"load_message_file",
+"parse_message_file_bytes",
+"new_bundle",
+"LanguageTag",
+"str",
+"LocalizeConfig",
+"Localizer",
+"localize",
+"new_localizer",
+"Message",
+"MessageFile",
+"parse_message_file_bytes",
+"PluralForm",
+"PluralCount",
+"str",
+"plural_count_int",
+"plural_count_string",
+"plural_form_for_language",
+"PluralForm.from",
+];
+
+var searchModuleData = [
+["A lightweight V port of nicksnyder's go-i18n package, providing simple message l","i18n.html"],
+];
+
+var searchData = [
+["i18n","","i18n.html#Bundle","struct "],
+["i18n","","i18n.html#Bundle.default_language","fn (Bundle)"],
+["i18n","","i18n.html#Bundle.language_tags","fn (Bundle)"],
+["i18n","","i18n.html#Bundle.add_messages","fn (Bundle)"],
+["i18n","","i18n.html#Bundle.load_message_file","fn (Bundle)"],
+["i18n","","i18n.html#Bundle.parse_message_file_bytes","fn (Bundle)"],
+["i18n","","i18n.html#new_bundle","fn "],
+["i18n","","i18n.html#LanguageTag","struct "],
+["i18n","","i18n.html#LanguageTag.str","fn (LanguageTag)"],
+["i18n","","i18n.html#LocalizeConfig","struct "],
+["i18n","","i18n.html#Localizer","struct "],
+["i18n","","i18n.html#Localizer.localize","fn (Localizer)"],
+["i18n","","i18n.html#new_localizer","fn "],
+["i18n","","i18n.html#Message","struct "],
+["i18n","","i18n.html#MessageFile","struct "],
+["i18n","","i18n.html#parse_message_file_bytes","fn "],
+["i18n","","i18n.html#PluralForm","enum "],
+["i18n","","i18n.html#PluralCount","struct "],
+["i18n","","i18n.html#PluralCount.str","fn (PluralCount)"],
+["i18n","","i18n.html#plural_count_int","fn "],
+["i18n","","i18n.html#plural_count_string","fn "],
+["i18n","","i18n.html#plural_form_for_language","fn "],
+["i18n","","i18n.html#PluralForm.from","fn "],
+];
+
