d4a172a7ce0a0e2793e7371298d150555901ea3d
scher <schermann470@outlook.com>
2026-09-15 20:13:17 +0300

    update test

--- template_test.v
+++ template_test.v
@@ -22,6 +22,26 @@ fn test_template_action_allows_outer_whitespace() {
 	assert rendered == 'Hello Ada'
 }
 
+fn test_template_action_allows_asymmetric_outer_whitespace() {
+	left_space := render_template('Hello {{ .Name}}', '', '', {
+		'Name': 'Ada'
+	}) or { panic(err) }
+	right_space := render_template('Hello {{.Name }}', '', '', {
+		'Name': 'Ada'
+	}) or { panic(err) }
+
+	assert left_space == 'Hello Ada'
+	assert right_space == 'Hello Ada'
+}
+
+fn test_template_action_rejects_internal_whitespace() {
+	render_template('Hello {{ . Name }}', '', '', {
+		'Name': 'Ada'
+	}) or { return }
+
+	assert false
+}
+
 fn test_custom_delimiters_replace_named_value() {
 	rendered := render_template('Hello <<.Name>>', '<<', '>>', {
 		'Name': 'Ada'
